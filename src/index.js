e5ba5ddf415bf37aff76005f5f6858b96671a559
Agustin Villalobos <guxtiv@gmail.com>
2018-03-18 16:09:51 -0600

    setting up graphcool into the react app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+    link: new HttpLink('https://api.graph.cool/simple/v1/cjexceli0056q0151wntjomdc'),
+    cache: new InMemoryCache(),
+});
+
+const withApolloProvider = Comp => (
+    <ApolloProvider client={client}>{Comp}</ApolloProvider>
+  );
+
+ReactDOM.render(withApolloProvider(<App />), document.getElementById('root'));
 registerServiceWorker();
